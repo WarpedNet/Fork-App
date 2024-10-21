3523e1502613c3e5f438be9337f3897c26ec3eb4
Callum <B00152224@mytudublin.ie>
2024-10-21 11:33:20 +0100

    whoops

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,6 +1,5 @@
 import { TouchableOpacity, View, Text } from 'react-native'
 import { React, useState } from 'react'
-import { router } from 'expo-router'
 import { SafeAreaView } from "react-native-safe-area-context"
 import CustomInputField from '../../components/CustomInputField'
 import { Link, router } from "expo-router"
@@ -29,9 +28,14 @@ const login = () => {
           <Text className="text-base text-white px-3 py-2">Don't have an account? <Link href="./register" className="text-secondary-100">Register Here</Link></Text>
 
           {/* temporary alert response to show user input based on log in button press */}
-          <TouchableOpacity className="bg-secondary rounded-full px-3 py-2" onPress={() => {alert("Username: "+formField.username+"\nPassword: "+formField.password)} }>
+          <TouchableOpacity className="bg-secondary rounded-full px-3 py-2" onPress={() => 
+            {alert("Username: "+formField.username+"\nPassword: "+formField.password)
+              router.push("../(tabs)/home")
+            }
+            }>
             <Text className="text-white text-2xl">Log In</Text>
           </TouchableOpacity>
+       </View>
       </View>
     </SafeAreaView>
   )
