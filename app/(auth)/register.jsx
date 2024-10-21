7f4c43c66dc16a959be41c805d6b7bc9011048b8
NotABusDriver <jakecallow42@gmail.com>
2024-10-21 10:26:27 +0100

    Added commenting to all files.

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -4,16 +4,30 @@ import { SafeAreaView } from "react-native-safe-area-context"
 import CustomInputField from '../../components/CustomInputField'
 import { router, Link } from "expo-router"
 
+// 
+// Register page for creating a new account
+// 
+
+// register function
 const register = () => {
   const [formField, setformField] = useState({username: "", password: ""})
   return (
     <SafeAreaView className="bg-primary h-full w-full">
+      {/* setting the view to centre */}
       <View className="h-full justify-center items-center space-y-4">
         <Text className="text-4xl text-white font-bold my-6">Register</Text>
+
+        {/* input fields for username and password */}
         <CustomInputField name="Username" placeholder="Username" changeText={(e) => setformField({...formField, username: e})} value={formField.username}/>
         <CustomInputField name="Password" placeholder="Password" changeText={(e) => setformField({...formField, password: e})} value={formField.password}/>
+
+        {/* setting the view to centre */}
         <View className="flex-col space-y-4 justify-center items-center">
+
+          {/* text and button for redirecting user back to login page if they have an account */}
           <Text className="text-base text-white px-3 py-2">Already have an account? <Link href="./login" className="text-secondary-100">Login Here</Link></Text>
+
+          {/* temporary alert response to show user input based on log in button press */}
           <TouchableOpacity className="bg-secondary rounded-full px-3 py-2" onPress={() => {alert("Username: "+formField.username+"\nPassword: "+formField.password)} }>
             <Text className="text-white text-2xl">Register</Text>
           </TouchableOpacity>
