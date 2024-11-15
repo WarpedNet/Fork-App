7a13900e6d2032a7ee2693ce6c507e293d0f7111
Callum <B00152224@mytudublin.ie>
2024-11-15 11:07:46 +0000

    uh oh

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -3,6 +3,7 @@ const tabLayout = () => {
     return (
         <Tabs>
             <Tabs.Screen name="home" options={{title: 'Home'}} />
+            <Tabs.Screen name="Forks" options={{title: 'Forks'}} />
         </Tabs>
         
     );
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -3,8 +3,9 @@ import {Text} from "react-native";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 export default function Home() {
   return (
-     
+    <SafeAreaView>
     <Text>Welcome to Fork!</Text>
+
   )
 }
  
