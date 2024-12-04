842097ea9094d797ddcf99f541fb4cfe1b4c251a
Callum <B00152224@mytudublin.ie>
2024-12-04 18:37:25 +0000

    ignore these changes i'm switching to a different branch to test something

--- app/(tabs)/forks.jsx
+++ app/(tabs)/forks.jsx
@@ -1,18 +1,23 @@
 //imports, i may need stylesheet but idk
 import React from "react";
-import {SafeAreaView} from "react-native-safe-area-context" 
-import {Text, FlatList, StyleSheet} from "react-native";
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context" 
+import {Text, FlatList, StyleSheet, ScrollView} from "react-native";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import ForkComponent from "../../components/ForkComponent";
 export default function Forks() {
   return (
+    <SafeAreaProvider>
     <SafeAreaView>
-    <ForkComponent>
-
-    </ForkComponent>
-
-
+      <ScrollView>
+        <ForkComponent />
+        <ForkComponent />
+        <ForkComponent />
+        <ForkComponent />
+        <ForkComponent />
+    </ScrollView>
     </SafeAreaView>
+    </SafeAreaProvider>
+
     
   )
 }
