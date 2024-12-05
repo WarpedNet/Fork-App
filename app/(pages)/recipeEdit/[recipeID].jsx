5de4306f4cada88c3ae05693e3bb87edcab06a0e
WarpedNet <102426484+WarpedNet@users.noreply.github.com>
2024-12-05 18:40:58 +0000

    Fixed imag

--- app/(pages)/recipeEdit/[recipeID].jsx
+++ app/(pages)/recipeEdit/[recipeID].jsx
@@ -6,7 +6,7 @@ import { Divider } from '@rneui/base'
 import { Button } from "@rneui/themed"
 import { router, useLocalSearchParams } from 'expo-router'
 import * as ImagePicker from 'expo-image-picker'
-import * as FileSystem from 'expo-file-system';
+import { Buffer } from "buffer"
 import * as ImageManipulator from "expo-image-manipulator"
 
 const recipeEditPage = () => {
@@ -36,9 +36,8 @@ const recipeEditPage = () => {
       aspect: [1,1]
     });
 
-  const resizedImage = await ImageManipulator.manipulateAsync(imageURI.assets[0].uri, [{resize: {width: 100, height: 100}}], {format: ImageManipulator.SaveFormat.PNG, base64:true})
-    // setRecipe({...recipe, icon: {base64:"data:image/png;base64"+resizedImage}})
-    setRecipe({...recipe, icon: resizedImage})
+  const resizedImage = await ImageManipulator.manipulateAsync(imageURI.assets[0].uri, [{resize: {width: 200, height: 200}}], {format: ImageManipulator.SaveFormat.PNG, base64:true})
+    setRecipe({...recipe, icon: resizedImage.base64})
   }
 
   const getBannerImage = async () => {
@@ -47,12 +46,11 @@ const recipeEditPage = () => {
       allowsEditing: true,
       quality: 1,
       allowsMultipleSelection: false,
-      aspect: [1,1]
+      aspect: [3,1]
     });
 
   const resizedImage = await ImageManipulator.manipulateAsync(imageURI.assets[0].uri, [{resize: {width: 600, height: 200}}], {format: ImageManipulator.SaveFormat.PNG, base64:true})
-    // setRecipe({...recipe, icon: {base64:"data:image/png;base64"+resizedImage}})
-    setRecipe({...recipe, bannerimg: resizedImage})
+    setRecipe({...recipe, bannerimg: resizedImage.base64})
   }
 
   useEffect(() => {
@@ -63,18 +61,22 @@ const recipeEditPage = () => {
   const saveRecipeLocal = async() => {
     const db = await SQLite.openDatabaseAsync("fork.db");
     await db.execAsync("CREATE TABLE IF NOT EXISTS Forks (ForkID INTEGER PRIMARY KEY NOT NULL, recipe_name TEXT, icon TEXT, recipe_desc TEXT, recipe_method TEXT, banner_img TEXT)")
-    await db.runAsync("INSERT INTO forks (recipe_name, icon, recipe_desc, recipe_method, banner_img) VALUES (?,?,?,?)", recipe.name, recipe.icon, recipe.desc, recipe.method, recipe.bannerimg)
+    console.log("data:image/png;base64,"+recipe.icon)
+    // await db.runAsync("INSERT INTO forks (recipe_name, icon, recipe_desc, recipe_method, banner_img) VALUES (?,?,?,?,?)", recipe.name, "data:image/png;base64,"+recipe.icon, recipe.desc, recipe.method, recipe.bannerimg)
+    const statement = await db.prepareAsync('INSERT INTO forks (recipe_name, icon, recipe_desc, recipe_method, banner_img) VALUES ($name, $icon, $desc, $method, $bannerimg)')
+    await statement.executeAsync({$name: recipe.name, $icon: recipe.icon, $desc: recipe.desc, $method: recipe.method, $bannerimg: recipe.banner_img})
+    // await db.runAsync("INSERT INTO forks (recipe_name, recipe_desc, recipe_method) VALUES (?,?,?)", recipe.name, recipe.desc, recipe.method)
   }
   
   return (
     <SafeAreaView>
       <View className="h-full w-full">
-          <TextInput value={recipe.name} onChangeText={(e) => setRecipe({...recipe, name: e})} title="RecipeName"/>
-          <Image source={recipe.icon} width={100} height={100}></Image>
+          <TextInput value={recipe.name} onChangeText={(e) => setRecipe({...recipe, name: e})} placeholder="RecipeName"/>
+          <Image source={{uri: "data:image/png;base64,"+recipe.icon}} width={100} height={100}></Image>
           <Button className=" dark:bg-secondary-300 w-[15vw] h-[15vw]" onPress={() => getIconImage()}><Text className="text-center text-xl">Icon Image</Text></Button>
-          <TextInput value={recipe.desc} onChangeText={(e) => setRecipe({...recipe, desc: e})} title="RecipeDesc"/>
-          <TextInput value={recipe.method} onChangeText={(e) => setRecipe({...recipe, method: e})} title="RecipeMethod"/>
-          <Image source={recipe.bannerimg} width={600} height={200}></Image>
+          <TextInput value={recipe.desc} onChangeText={(e) => setRecipe({...recipe, desc: e})} placeholder="RecipeDesc"/>
+          <TextInput value={recipe.method} onChangeText={(e) => setRecipe({...recipe, method: e})} placeholder="RecipeMethod"/>
+          <Image source={{uri: "data:image/png;base64,"+recipe.bannerimg}} width={600} height={200}></Image>
           <Button className=" dark:bg-secondary-300 w-[15vw] h-[15vw]" onPress={() => getBannerImage()}><Text className="text-center text-xl">Banner Image</Text></Button>
           {/* Bottom Bar */}
           <View className="h-[10vh]">
---
+++ app/(pages)/recipeShow/[recipeID].jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const recipeShow = () => {
+
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  )
+}
+
+export default recipeShow
