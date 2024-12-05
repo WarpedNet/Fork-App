0eb457c1fbe8ff3bc1852762f33ea559f962123b
Callum <B00152224@mytudublin.ie>
2024-12-05 20:00:55 +0000

    eas functionality for local building

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,8 @@
 import { Stack } from "expo-router"
 import "../globals.css"
-import { StyleSheet } from "nativewind"
 // 
 // Stack layout for the default pages.
 // 
-
-//styling for the recipe form
-
-const recipeForm = StyleSheet.create({
-    input: {
-        height: 30,
-        margin: 10,
-        borderWidth: 1,
-        padding: 10,
-    },
-});
 
 
 
