b14886a071cd347b9f0f97b98aa114b03025cb13
WarpedNet <102426484+WarpedNet@users.noreply.github.com>
2025-05-01 22:59:19 +0100

    Disabled a page since it is unnecessary and also added an upload button with its functionality

--- app/(pages)/recipeShow/[recipeID].jsx
+++ app/(pages)/recipeShow/[recipeID].jsx
@@ -5,6 +5,7 @@ import { Divider } from '@rneui/base'
 import { router, useLocalSearchParams } from 'expo-router'
 import * as SQLite from 'expo-sqlite'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import * as SecureStore from 'expo-secure-store'
 
 const recipeShow = () => {
 
@@ -35,6 +36,39 @@ const recipeShow = () => {
     }
   }
 
+  async function uploadRecipe() {
+    try {
+      const api_address = process.env.EXPO_PUBLIC_API_ADDRESS
+      const userToken = await SecureStore.getItemAsync("FORK_USER_TOKEN")
+      const response = await fetch(`${api_address}/fork`, {
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          token: userToken,
+          parentID: null,
+          recipeName: recipe.name,
+          recipeDesc: recipe.description,
+          recipeMethod: recipe.method,
+          bannerImg: recipe.banner,
+          icon: recipe.icon,
+        }),
+      });
+      if (response.status == 401) {
+        alert("Login token expired");
+        router.push("login");
+      }
+      if (response.status == 200) {
+        alert("Fork Created!");
+      }
+    }
+    catch (error) {
+      alert("Error uploading recipe: "+error);
+    }
+  }
+
   useEffect(() => {
     getRecipe()
   }, [])
@@ -56,6 +90,7 @@ const recipeShow = () => {
               <Divider width={3} color="black" className="align-bottom"></Divider>
               <View className="flex-row gap-8 justify-center items-center mb-4 mt-2">
                 <Button className=" dark:bg-secondary-300 w-[15vw] h-[15vw]" onPress={() => router.push(`../recipeEdit/${recipeID}`)}><Text className="text-center text-xl">Edit</Text></Button>
+                <Button className=" dark:bg-secondary-300 w-[15vw] h-[15vw]" onPress={() => uploadRecipe()}><Text className="text-center text-xl">Upload</Text></Button>
                 <Button className=" dark:bg-secondary-300 w-[15vw] h-[15vw]" onPress={() => deleteRecipe()}><Text className="text-center text-xl">Delete</Text></Button>
               </View>
             </View>
